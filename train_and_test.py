89a00539a19798c5f15a0a3cf1cca384b8437c5c
Nicole Story <nicole7story@gmail.com>
2019-04-20 12:22:21 -0500

    Read in fasta files

--- train_and_test.py
+++ train_and_test.py
@@ -6,15 +6,16 @@ import utils
 
 def main():
     # Parse args and split data into training and testing
-    pssm_list, tm_align_list, pssm_dir, tm_align_dir = parse_args()
+    pssm_list, tm_align_list, fasta_list, pssm_dir, tm_align_dir, fasta_dir = parse_args()
     pssm_train = sample(pssm_list, int(0.75 * len(pssm_list)))
     pssm_test = [pssm for pssm in pssm_list if pssm not in pssm_train]
     # train(pssm_train, pssm_dir, tm_align_dir)
 
     # TODO: For testing purposes, remove later
-    feature_matrix = build_feature_matrix(pssm_train, pssm_dir, None, tm_align_dir)
+    feature_matrix = build_feature_matrix(pssm_train, pssm_dir, fasta_dir, tm_align_dir)
 
 
+# TODO: Edit error message
 err_msg = '''
 Please enter two directory names (absolute paths)
 containing sequences for linear regression training data
@@ -24,7 +25,7 @@ followed by the path to the tmalign files.'''
 
 
 def parse_args():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print(err_msg)
         sys.exit()
 
@@ -32,13 +33,14 @@ def parse_args():
         # Get the lists of pssm and rr file names
         pssm = utils.read_directory_contents(sys.argv[1], '.pssm')
         tm_align = utils.read_directory_contents(sys.argv[2], '_tmalign')
+        fasta = utils.read_directory_contents(sys.argv[3], '.fasta')
     except:
         # Given paths are not valid directories
         print(err_msg)
         sys.exit()
 
     # Return list of pssm & tm_align files, and their parent directories
-    return pssm, tm_align, sys.argv[1], sys.argv[2]
+    return pssm, tm_align, fasta, sys.argv[1], sys.argv[2], sys.argv[3]
 
 
 if __name__ == '__main__':
