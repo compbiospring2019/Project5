41ca6fb7cbccf3c1558c394e01a2aa2f0ee8b11f
Nicole Story <nicole7story@gmail.com>
2019-04-20 12:53:40 -0500

    Removed training and testing code from secondary_structure

--- secondary_structure/ss_utils.py
+++ secondary_structure/ss_utils.py
@@ -7,32 +7,6 @@ this_script = os.path.abspath(__file__)
 parent_directory = os.path.dirname(this_script)
 
 
-def read_sequence(file_path, dir=None):
-    # Read a biological sequence or RSA sequence from a file
-    if file_path is None:
-        return None
-    sequence = ''
-    if dir:
-        file_path = os.path.join(dir, file_path)
-    with open(file_path, 'r') as f:
-        # Ignore the title line
-        title = f.readline()
-        for line in f:
-            sequence += line.strip()
-
-    return sequence.upper()
-
-
-def read_directory_contents(path, file_extension):
-    if not os.path.isdir(path):
-        # This is not a valid directory!
-        raise Exception('Not a valid directory!')
-
-    # Return a list of files with the file extension
-    ls_dir = os.listdir(path)
-    return [file_name for file_name in ls_dir if file_name.endswith(file_extension)]
-
-
 def read_pssm(file_path, dir=None):
     if dir:
         file_path = os.path.join(dir, file_path)
@@ -69,21 +43,6 @@ def read_dist(file_path, dir=None):
         prior = float(f.readline())
         dists = [[float(x) for x in line] for line in f]
     return prior, dists
-
-
-def split_files(pssm_list, ss_list):
-    # Divide the .pssm files into training and testing sets
-    test_correct_pssm_files(pssm_list, ss_list)
-    pssm_train = sample(pssm_list, int(0.75 * len(pssm_list)))
-    pssm_test = [pssm_name for pssm_name in pssm_list if pssm_name not in pssm_train]
-    return pssm_train, pssm_test
-
-
-def test_correct_pssm_files(pssm_list, ss_list):
-    # Make sure each .pssm has a corresponding .ss
-    for pssm_name in pssm_list:
-        if pssm_name.replace('.pssm', '.ss') not in ss_list:
-            raise Exception('PSSM files don\'t match up with .ss files: {}'.format(pssm_name))
 
 
 def compute_percentages(ss_prediction):
