30c2b5d95af4ac681070f030ce307f1b6c2ea9ee
Nicole Story <nicole7story@gmail.com>
2019-04-20 22:02:12 -0500

    Read the model in only once

--- solvent_accessibility/classify_sequence.py
+++ solvent_accessibility/classify_sequence.py
@@ -3,13 +3,14 @@ import solvent_accessibility.sa_utils as utils
 from solvent_accessibility.decision_tree import DecisionTree
 
 
+# Read in the model
+json_model = utils.read_json()
+
+
 def classify(test_file, test_file_dir=None):
     if test_file_dir:
         test_file = os.path.join(test_file_dir, test_file)
     test_sequence = utils.read_sequence(test_file)
-
-    # Read in the model
-    json_model = utils.read_json()
 
     # Classify the test sequence
     predicted_rsa = DecisionTree.classify_sequence(test_sequence, json_model)
